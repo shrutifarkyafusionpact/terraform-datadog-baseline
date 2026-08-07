620804ba53c9d72fbe60c95543395812f4c1d3c5
shrutifarkyafusionpact <shruti.farkya@fusionpact.com>
2026-08-07 10:24:21 +0530

    Add variables for service, environment, team, and owner

---
+++ modules/tagging/variables.tf
@@ -0,0 +1,23 @@
+variable "service" {
+
+  type = string
+
+}
+
+variable "environment" {
+
+  type = string
+
+}
+
+variable "team" {
+
+  type = string
+
+}
+
+variable "owner" {
+
+  type = string
+
+}
