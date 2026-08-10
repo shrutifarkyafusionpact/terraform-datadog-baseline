e651af615e2104eb5865972d86effea6c99aa6aa
shrutifarkyafusionpact <shruti.farkya@fusionpact.com>
2026-08-10 13:31:33 +0530

    Add Datadog monitors for Lambda duration and throttles

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -15,3 +15,39 @@ resource "datadog_monitor" "errors" {
   no_data_timeframe = var.no_data_timeframe
   renotify_interval = var.renotify_interval
 }
+
+resource "datadog_monitor" "duration" {
+
+  name = "${var.environment} Lambda High Duration"
+
+  type = "metric alert"
+
+  query = "avg(last_5m):avg:aws.lambda.duration{functionname:${var.lambda_function_name}} > ${var.duration_threshold}"
+
+  message = local.common_message
+
+  tags = var.tags
+
+  evaluation_delay   = var.evaluation_delay
+  notify_no_data     = var.notify_no_data
+  no_data_timeframe  = var.no_data_timeframe
+  renotify_interval  = var.renotify_interval
+}
+
+resource "datadog_monitor" "throttles" {
+
+  name = "${var.environment} Lambda Throttles"
+
+  type = "metric alert"
+
+  query = "sum(last_5m):sum:aws.lambda.throttles{functionname:${var.lambda_function_name}} > ${var.throttle_threshold}"
+
+  message = local.common_message
+
+  tags = var.tags
+
+  evaluation_delay   = var.evaluation_delay
+  notify_no_data     = var.notify_no_data
+  no_data_timeframe  = var.no_data_timeframe
+  renotify_interval  = var.renotify_interval
+}
